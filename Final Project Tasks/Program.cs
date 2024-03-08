52b5a9896514ee5389f4dbd253f83d3e3828e25c
lucka <luckagvazava@gmail.com>
2024-03-08 22:27:06 +0400

    calculator does not turn off anymore after attempts.

--- Final Project Tasks/Program.cs
+++ Final Project Tasks/Program.cs
@@ -4,75 +4,90 @@
     {
         static void Main(string[] args)
         {
+            string condition = string.Empty;
+            bool program = true;
 
-            try
+            while (program)
             {
 
 
-                while (true)
-
-
+                try
                 {
 
-                    Console.WriteLine("Enter the first number: ");
-                    double num1 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter the second number: ");
-                    double num2 = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine("Enter the operation [+ - * /]: ");
-                    char operation = Convert.ToChar(Console.ReadLine());
-
-                    double result = 0;
+                    while (true)
 
 
-                    switch (operation)
                     {
-                        case '+':
-                            result = num1 + num2;
-                            break;
+
+                        Console.WriteLine("Enter the first number: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter the second number: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter the operation [+ - * /]: ");
+                        char operation = Convert.ToChar(Console.ReadLine());
+
+                        double result = 0;
 
 
-                        case '-':
-                            result = num1 - num2;
-                            break;
+                        switch (operation)
+                        {
+                            case '+':
+                                result = num1 + num2;
+                                break;
 
 
-                        case '*':
-                            result = num1 * num2;
-                            break;
+                            case '-':
+                                result = num1 - num2;
+                                break;
+
+
+                            case '*':
+                                result = num1 * num2;
+                                break;
 
 
 
 
-                        case '/':
+                            case '/':
 
 
 
-                            if (num2 != 0)
-                            {
-                                result = num1 / num2;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Cannot divide by zero.");
-                                return;
-                            }
-                            break;
+                                if (num2 != 0)
+                                {
+                                    result = num1 / num2;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cannot divide by zero.");
+                                    return;
+                                }
+                                break;
 
 
+                        }
+
+                        Console.WriteLine($"Result: {result}");
                     }
-
-                    Console.WriteLine($"Result: {result}");
                 }
-            }
-            catch (Exception e)
-            {   
-                Console.WriteLine(e.Message);
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+               
+                Console.WriteLine("do you want to continue? Enter [yes] or [no]");
+                condition = Console.ReadLine();
+               
+                if (condition.Equals("No", StringComparison.OrdinalIgnoreCase))
+
+                {
+                    program = false;
+                }
             }
 
         }
-
     }
 }
 
